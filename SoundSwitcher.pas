45103c6c733d95c3e70d8fc7d1f894f3af25d077
Mrgnstrn <demonmind@mail.ru>
2014-08-31 23:57:42 +0600

    Loading2Git

---
+++ SoundSwitcher.pas
@@ -0,0 +1,29 @@
+program SoundSwitcher;
+
+uses
+  Windows,
+  Vcl.Forms,
+  Unit1 in 'Unit1.pas' {frmMain},
+  Vcl.Themes,
+  Vcl.Styles;
+
+//Vcl.Themes,
+  //Vcl.Styles;
+
+
+{.$R *.res}
+
+begin
+
+  CreateMutex(nil, True, '{DB9535CF-4065-45A4-8581-141568484732}');
+if GetLastError = ERROR_ALREADY_EXISTS then
+begin
+  MessageBox(0,'Program is already started', 'SoundSwitcher', MB_OK + MB_ICONERROR + MB_DEFBUTTON1);
+  Exit;
+end;
+  Application.Initialize;
+  Application.ShowMainForm:=False;
+  Application.Title := 'SoundSwitcher';
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
